fbbbd85c61dca83fc56b345e8bb70667d65cb728
Nazrul islam <jacksparrow.alex@gmail.com>
2021-04-06 16:48:07 +0600

    deleting data through click in ui done, this click also remove datat from firebase database

--- script.js
+++ script.js
@@ -5,20 +5,35 @@ let form = document.querySelector("#add-cafe-form")
 
 const renderDoc = doc => {
     let li = document.createElement("li");
+
     let name = document.createElement("span")
     let city = document.createElement("city")
+    let cross = document.createElement("div")
     li.setAttribute("data-id", doc.id)
+
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = "X"
+
     li.appendChild(name)
     li.appendChild(city)
+
     cafeList.appendChild(li)
+    li.appendChild(cross)
+    cross.addEventListener("click", (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute("data-id")
+        db.collection("cafes").doc(id).delete()
+    })
+
 
 }
 db.collection("cafes").get().then((snapshot) => {
     snapshot.docs.forEach((doc) => {
         renderDoc(doc)
     })
+
+
 })
 
 form.addEventListener("submit", (e) => {
